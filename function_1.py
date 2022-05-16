f491ec398550b797b206b17d9516313ad0542ded
Sam Fedosenko <Co0mrad@yandex.ru>
2022-05-16 23:27:13 +0500

    correct function_1

--- function_1.py
+++ function_1.py
@@ -1,8 +1,5 @@
 def get_sum(one, two, delimiter ="&"):
-        if delimiter != ("&"):
-            raise ValueError("Тут неверный символ")
-        else:
-            allsumm = (one+delimiter+two)
+        allsumm = (one+delimiter+two)
         return(allsumm)
 x = get_sum("Learn","python")
 x = str(x.upper())
